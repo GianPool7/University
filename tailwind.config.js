59a354ac51bbf3c1a15043010e24f8726c087ff5
gianpool <gianpoolalvinocantaro@gmail.com>
2024-12-20 16:14:04 -0500

    realizando mi primera web con astro university

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js}", // Busca en todos los archivos .astro, .html y .js dentro de src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
